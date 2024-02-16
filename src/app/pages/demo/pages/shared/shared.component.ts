50243a63796fc1cdfe1f324f620ddd8ce9574d6d
Madikhan Sadykov <91040867+MadikhanSadykov@users.noreply.github.com>
2024-02-16 18:21:24 +0600

    019 created select component

--- src/app/pages/demo/pages/shared/shared.component.ts
+++ src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {regex, regexErrors} from "@app/shared";
 
 @Component({
   selector: 'app-shared',
@@ -9,22 +10,47 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class SharedComponent implements OnInit {
 
   form: FormGroup;
+  isInline: boolean;
+  regexErrors = regexErrors;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.isInline = true;
+  }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      input: [null]
+      input: [null, {
+        updateOn: 'blur',
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern(regex.numbers)
+        ]
+      }],
+      password: [null, {
+        updateOn: 'blur',
+        validators: [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.pattern(regex.password)
+        ]
+      }]
     });
   }
 
   onPatchValue(): void {
     this.form.patchValue({
-      input: 'test'
+      input: 'test',
+      password: 'test'
     });
   }
 
   onSubmit(): void {
     console.log('Submit!');
   }
+
+  onToggleInline() {
+    this.isInline = !this.isInline;
+  }
+
 }
--- src/app/shared/controls/index.ts
+++ src/app/shared/controls/index.ts
@@ -1,4 +1,6 @@
 export * from './controls.module';
 export * from './input/input.module';
 export * from './form-field/form-field.module';
+export * from './password/password.module';
+export * from './select/select.module';
 
